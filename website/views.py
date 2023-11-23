a55f28c8ca60dc94834fe7bcf30c32b4cfdbc869
Sergio Rosa <scarlosjazz@gmail.com>
2023-11-23 08:33:00 -0500

    Authemticate - LoggedIn

--- website/views.py
+++ website/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib.auth  import authenticate, login, logout
+from django.contrib import messages
+
+
 def home(request):
-  return render(request, 'home.html',{})
+  # check to seee if logging in
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    
+    # Authemticate
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+      login(request, user)
+      messages.success(request, "Welcome, You are Logged In!")
+      return redirect('home')
+    else: 
+      messages.success(request, "There was an error Logged In, Please try again...")
+      return redirect('home')
+  else: 
+      return render(request, 'home.html',{})
+
+
+
+# def login_user(request): 
+#   pass
+ 
+    
+def logout_user(request): 
+  pass
